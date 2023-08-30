06d49e89e4f415290e8ba7d6ac5fcbe2091b1fa5
gotokouichi <49635114+gotokouichi@users.noreply.github.com>
2023-08-30 10:45:20 +0900

    Initial commit

---
+++ api/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import { applyServerSettings } from './settings';
+
+const app = express();
+const port = 8000;
+
+// Middlewares and settings
+applyServerSettings(app);
+
+// ↓↓↓ バックエンド処理を記述して実際に開発してみましょう！！
+
+// ↑↑↑ バックエンド処理を記述して実際に開発してみましょう！！
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
